9e49f434f11870068568ba04612a940cb22802d3
jose antonio <joseantonioalmeida217@gmail.com>
2026-07-17 18:34:14 -0300

    Criei as funções cadastrar e listar livros

---
+++ README.md
@@ -0,0 +1,12 @@
+Exercício: Sistema de Gerenciamento de Biblioteca 📚
+Objetivo
+
+Programa que gerencie livros utilizando:
+
+Funções
+Listas
+Dicionários
+Laços de repetição
+Condições
+Busca
+Organização do código
--- src/gerenciamento_de_biblioteca/main.py
+++ src/gerenciamento_de_biblioteca/main.py
@@ -1,5 +1,80 @@
-def main() -> int:
-    return 1
+from typing import TypedDict
+
+
+class Livro(TypedDict):
+    id: int
+    titulo: str
+    autor: str
+    ano: int
+
+
+def menu() -> None:
+    biblioteca: list[Livro] = []
+    while True:
+        print("\n=== Biblioteca ===")
+        print("1 - Cadastrar livros")
+        print("2 - Listar livros")
+        print("99 - Sair")
+
+        opcao = input("Escolha: ").strip()
+
+        if opcao == "1":
+            cadastrar_livro(biblioteca)
+        elif opcao == "2":
+            listar_livros(biblioteca)
+        elif opcao == "99":
+            print("Até logo!")
+            break
+        else:
+            print("Opção inválida.")
+
+
+def cadastrar_livro(biblioteca: list[Livro]) -> None:
+    while True:
+        titulo = input("Digite o título do livro: ").strip()
+        if not titulo:
+            continue
+        autor = input("Digite o autor do livro: ").strip()
+        if not autor:
+            continue
+
+        try:
+            ano = int(input("Digite o ano do livro: "))
+        except ValueError:
+            print("Digite um ano válido.")
+            continue
+
+        biblioteca.append(
+            {
+                "id": len(biblioteca) + 1,
+                "titulo": titulo,
+                "autor": autor,
+                "ano": ano,
+            }
+        )
+        continuar = (
+            input("Deseja cadastrar mais livros? (SIM ou NÃO): ").strip().upper()
+        )
+        if continuar.startswith("S"):
+            continue
+
+        break
+
+
+def listar_livros(biblioteca: list[Livro]) -> None:
+    print("=" * 30)
+    print("=" * 11, "LIVROS", "=" * 11)
+    for livro in biblioteca:
+        print("ID", livro["id"])
+        print("titulo", livro["titulo"])
+        print("autor", livro["autor"])
+        print("ano", livro["ano"])
+        print("-" * 30)
+    print("=" * 30)
+
+
+def main() -> None:
+    menu()
 
 
 if __name__ == "__main__":
