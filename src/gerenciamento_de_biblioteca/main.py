11c78f67e938a4c3dd8946dc830e6881db5a820b
jose antonio <joseantonioalmeida217@gmail.com>
2026-07-17 21:04:25 -0300

    Buscar livro por titulo

--- src/gerenciamento_de_biblioteca/main.py
+++ src/gerenciamento_de_biblioteca/main.py
@@ -14,6 +14,12 @@ def menu() -> None:
         print("\n=== Biblioteca ===")
         print("1 - Cadastrar livros")
         print("2 - Listar livros")
+        print("3 - Procurar livro pelo título")
+        print("4 - Editar livro")
+        print("5 - Remover livro")
+        print("6 - Emprestar livro")
+        print("7 - Devolver livro")
+        print("8 - Mostrar quantidade de livros")
         print("99 - Sair")
 
         opcao = input("Escolha: ").strip()
@@ -22,6 +28,8 @@ def menu() -> None:
             cadastrar_livro(biblioteca)
         elif opcao == "2":
             listar_livros(biblioteca)
+        elif opcao == "3":
+            buscar_livro_pelo_titulo(biblioteca)
         elif opcao == "99":
             print("Até logo!")
             break
@@ -61,16 +69,36 @@ def cadastrar_livro(biblioteca: list[Livro]) -> None:
         break
 
 
+def mostrar_livro(livro: Livro) -> None:
+    print("ID", livro["id"])
+    print("titulo", livro["titulo"])
+    print("autor", livro["autor"])
+    print("ano", livro["ano"])
+
+
 def listar_livros(biblioteca: list[Livro]) -> None:
     print("=" * 30)
     print("=" * 11, "LIVROS", "=" * 11)
     for livro in biblioteca:
-        print("ID", livro["id"])
-        print("titulo", livro["titulo"])
-        print("autor", livro["autor"])
-        print("ano", livro["ano"])
+        mostrar_livro(livro)
         print("-" * 30)
     print("=" * 30)
+
+
+def buscar_livro_pelo_titulo(biblioteca: list[Livro]) -> None:
+    titulo_digitado = input("Digite o titulo: ").strip()
+
+    encontrado = False
+    for livro in biblioteca:
+        if titulo_digitado.lower() in livro["titulo"].lower():
+            encontrado = True
+
+            print("-" * 30)
+            mostrar_livro(livro)
+
+    print("-" * 30)
+    if not encontrado:
+        print("Livro não encontrado.")
 
 
 def main() -> None:
