cdeefe3a55cd0630fa0bcc3dbeb7c24d195d1e9f
jose antonio <joseantonioalmeida217@gmail.com>
2026-07-31 22:22:52 -0300

    feat: Coverage, Taskipy, TestContainer[postgres], factoryboy e freezegun colocados no projeto.

--- src/gerenciamento_de_biblioteca/main.py
+++ src/gerenciamento_de_biblioteca/main.py
@@ -1,18 +1,9 @@
-import asyncio
-import sys
 from http import HTTPStatus
-from typing import TYPE_CHECKING
 
 from fastapi import FastAPI
 
 from gerenciamento_de_biblioteca.routers import books
 from gerenciamento_de_biblioteca.schemas import Message
-
-if TYPE_CHECKING:
-    pass
-
-if sys.platform == "win32":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 app = FastAPI(title="Gerenciamento de biblioteca API")
 
