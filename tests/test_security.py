7c306317fb5971d783560c4992cc0485583b5cdc
jose antonio <joseantonioalmeida217@gmail.com>
2026-08-04 18:48:20 -0300

    test: Security, e coverage 100%

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,5 +1,8 @@
+from http import HTTPStatus
+
 import pytest
-from jwt import decode
+from freezegun import freeze_time
+from jwt import decode, encode
 
 from gerenciamento_de_biblioteca.security import (
     create_access_token,
@@ -14,3 +17,56 @@ async def test_jwt(settings):
 
     assert decoded["mensagem"] == "test"
     assert "exp" in decoded
+
+
+def test_invalid_jwt_exception_decode_error(client):
+    response = client.get(
+        "/users/", headers={"Authorization": "Bearer token-invalid"}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {"detail": "Could not validate credentials."}
+
+
+def test_token_invalid_not_subject_email(client):
+    token_sem_sub = create_access_token(data={})
+    response = client.get(
+        "/users/", headers={"Authorization": f"Bearer {token_sem_sub}"}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {"detail": "Could not validate credentials."}
+
+
+def test_token_expired_after_time(client, user):
+    with freeze_time("2026-08-04 12:00:00"):
+        response = client.post(
+            "/auth/token/",
+            data={"username": user.email, "password": user.clean_password},
+        )
+
+        token_expired = response.json()["access_token"]
+
+        assert response.status_code == HTTPStatus.OK
+
+    with freeze_time("2026-08-04 12:30:00"):
+        response = client.patch(
+            f"/users/{user.id}/",
+            data={"username": user.email, "password": user.clean_password},
+            headers={"Authorization": f"Bearer {token_expired}"},
+        )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {"detail": "Could not validate credentials."}
+
+
+def test_get_current_user_not_found(client, settings):
+    data = {"sub": "nao_existo@example.com"}
+    token = encode(data, settings.SECRET_KEY, settings.ALGORITHM)
+
+    response = client.get(
+        "/users/", headers={"Authorization": f"Bearer {token}"}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {"detail": "Could not validate credentials."}
