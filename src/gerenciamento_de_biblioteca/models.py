cae2485061580de176057e145121e1a14de0d859
jose antonio <joseantonioalmeida217@gmail.com>
2026-08-04 21:19:57 -0300

    feat: add user relation to books

--- src/gerenciamento_de_biblioteca/models.py
+++ src/gerenciamento_de_biblioteca/models.py
@@ -1,26 +1,9 @@
 from datetime import datetime  # noqa: TC003
 
-from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column, registry
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
 
 table_registry = registry()
-
-
-@table_registry.mapped_as_dataclass
-class Book:
-    __tablename__ = "book"
-
-    id: Mapped[int] = mapped_column(init=False, primary_key=True)
-    title: Mapped[str] = mapped_column(unique=True)
-    author: Mapped[str]
-    year: Mapped[int]
-    available: Mapped[bool] = mapped_column(default=True)
-    created_at: Mapped[datetime] = mapped_column(
-        init=False, server_default=func.now()
-    )
-    updated_at: Mapped[datetime] = mapped_column(
-        init=False, server_default=func.now(), onupdate=func.now()
-    )
 
 
 @table_registry.mapped_as_dataclass
@@ -31,6 +14,29 @@ class User:
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
+    books: Mapped[list["Book"]] = relationship(
+        init=False,
+        cascade="all, delete-orphan",
+        lazy="selectin",
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), onupdate=func.now()
+    )
+
+
+@table_registry.mapped_as_dataclass
+class Book:
+    __tablename__ = "book"
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    title: Mapped[str] = mapped_column(unique=True)
+    author: Mapped[str]
+    year: Mapped[int]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    available: Mapped[bool] = mapped_column(default=True)
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
